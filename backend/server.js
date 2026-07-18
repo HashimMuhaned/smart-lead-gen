8af6bc541061589b5e2ec226416df4d7452b2dbe
HashimMuhaned <hashimcode123@gmail.com>
2026-07-18 11:55:54 +0400

    still fixing cors, using now reverse proxy and vercel.json in the frontend not backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const campaignRoutes = require("../src/routes/campaigns");
+const businessRoutes = require("../src/routes/businesses");
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Wire up routes directly
+app.use("/api/campaigns", campaignRoutes);
+app.use("/api/businesses", businessRoutes);
+
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+}
+
+module.exports = app;
